509fdb08755c3497fe375336ef08e382e41848c1
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2019-07-03 19:32:20 -0300

    Pegando apartamentos do state da listagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
 
+import logo from './logo.svg';
+
 import Card from './components/Card';
 
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      
-      <h1>Apartamentos favoritos</h1>
-      
-      <div className="listagem-cards">
-      
-        <Card />
-         
-          
-      </div>
+export default class App extends React.Component < any, any >{
 
-    </div>
-  );
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      apartamentos: [
+        {
+          title: "Apartamento 1",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, iure commodi animi impedit corporis laudantium inventore excepturi consequatur eos aliquam ipsum doloremque corrupti, atque illum error debitis dicta dignissimos cum! ",
+          logo: logo
+        },
+        {
+          title: "Apartamento 2",
+          description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, iure commodi animi impedit corporis laudantium inventore excepturi consequatur eos aliquam ipsum doloremque corrupti, atque illum error debitis dicta dignissimos cum! ",
+          logo: logo
+        },
+        {
+          title: "Casa do Vinicoi",
+          description: "Descrição do apartamento do vinicoi",
+          logo: logo
+        }
+      ]
+
+    };
+
+
+  }
+
+  render() {
+
+    const { apartamentos } = this.state;
+
+    return (
+      <div className="App">
+        
+        <h1>Apartamentos favoritos</h1>
+        
+        <div className="listagem-cards">
+        
+          {apartamentos.map(function (apartamento: any) {
+            return (
+              <Card title={apartamento.title} description={apartamento.description} logo={apartamento.logo}/>
+            );
+          })}
+           
+            
+        </div>
+  
+      </div>
+    );
+  }
+
+
+  
 }
 
-export default App;
+
